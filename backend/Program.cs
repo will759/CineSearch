30750851d5c9400772cc35d955d9a9a13296f04c
will759 <140643798+will759@users.noreply.github.com>
2026-02-24 01:13:35 -0300

    Vercel

--- backend/Program.cs
+++ backend/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "https://cine-search-livid.vercel.app/")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
